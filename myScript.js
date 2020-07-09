45fb8f180657d12ec93b3b59849a2327fda2a74b
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2020-07-08 21:53:36 -0600

    Reworked for loop to append cards to page.

--- myScript.js
+++ myScript.js
@@ -16,11 +16,10 @@ var key = '63aa044b0ff611c7351bd3f3fe384feb';
 $('#search-button').on('click', function() {
 	event.preventDefault();
 	var cityName = $('#user-search').val();
-	console.log(cityName);
 	$('#user-search').val("");
 	weatherSearch(cityName);
 	weatherForecast(cityName);
-
+	$('#my-cities')
 
 });
 
@@ -31,16 +30,14 @@ function weatherSearch(cityName) {
 		method: "GET",
 		dataType: "json"	
 	}).then(function(response) {
-		var results =response;
-		console.log(results);
+		var results = response;
 		var currentCity = $('<h2>').text(cityName + " date");
 		var currentTemp = $('<p>').text(results.main.temp);
-		console.log(results.main.temp);
 		var currentHum = $('<p>').text(results.main.humidity);
-		console.log(results.main.humidity);
 		var currentWind = $('<p>').text(results.wind.speed);
 		var currentIcon = $('<img>').attr('src','https://openweathermap.org/img/w/' + results.weather[0].icon + '.png');
-		$('#current-header').html(currentCity,currentIcon); //Set first letter to uppercase
+		$('#current-header').html(currentCity); //Set first letter to uppercase
+		$('#current-header').append(currentIcon);
 		$('#current-temp').append(currentTemp);
 		$('#current-humidity').append(currentHum);
 		$('#current-wind').append(currentWind);
@@ -57,14 +54,33 @@ function weatherForecast(cityName) {
 }).then(function(response) {
 	var results = response;
 	console.log(results);
+	// var days = results.list.filter(function (forecast){
+	// 	return forecast.dt_txt.includes("12:00:00")
+	// });
+	// console.log(days);
+
 
 	for (i=0;i<results.list.length;i++) {
 		var icon = $('<img>').attr('src','https://openweathermap.org/img/w/' + results.weather[i].icon + '.png');
-		var date = $('<p>').text(results.list[i].dt_text);
+		var date = $('<p>').text(days);
 		console.log(results.list[0].dt_text);
 		var humidity = $('<p>').text(results.list[i].main.humidity);
-		var infoCard = $('<h5 class="card-header>').text(date).append('div class="card-body"').html('<p>').text(icon).append('<div class="card-footer"').text(humidity);
-		$('#forecast-container').append(infoCard);
+		var city = results.city.city.name;
+		var temp = results.list[i].main.temp_max;
+		var weatherCard = $('<div>').attr({
+			'class': 'card text-white bg-primary mb-3',
+			'style': 'max-width:18rem'
+		})
+		$('#forecast-deck').append(weatherCard);
+		var cardHead = $('<div>').attr('class','card-header');
+		var cardBody = $('<div>').attr('class','card-body');
+		var cardTitle = $('<h5>').attr('class','card-title');
+		var cardText = $('<p>').attr('class','card-text');
+			cardTitle.text(icon);
+			cardText.text(weather + '<br><br>' + 'High: ' (temp) + 'F' + '<br><br>' + 'Humidity: ' + humidity);
+			weatherCard.append(cardHead,cardBody);
+			cardBody.append(cardTitle,cardText);
+			cardHead.text(city);
 	}
 
 });
